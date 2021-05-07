933847f4150167e4da3b852e1d69e61a0fd00faa
SaitoYoshiko <yo.saito3759@icloud.com>
2021-05-07 16:27:23 +0900

    アロー関数が難しいので、取り組みましたが、回答を見ました。

--- .bash_history
+++ .bash_history
@@ -5,3 +5,7 @@ bin/hubot
 exit
 yarn install
 exit
+env const lots = ['大吉', '吉', '中吉', '末吉', '凶'];
+env HUBOT_SLACK_TOKEN=xoxb-2025627461543-2037402301445-dMf72HmZLpKdJ9Fbzl7BlOsF bin/hubot adapter slack
+env HUBOT_SLACK_TOKEN=xoxb-2025627461543-2037402301445-dMf72HmZLpKdJ9Fbzl7BlOsF bin/hubot --adapter slack
+exit
--- scripts/hello.js
+++ scripts/hello.js
@@ -1,7 +1,24 @@
 'use strict';
+/**
+ * hello>と呼びかけるとslack botが応答してくれる関数
+ * @param {*} robot 
+ */
 module.exports = robot => {
   robot.hear(/hello>/i, msg => {
     const user_id = msg.message.user.id;
     msg.send(`Hello, <@${user_id}>`);
   });
+
+
+/**
+ * omikuji>と入力すると、ランダムでおみくじの結果を教えてくれる関数
+ * 
+ */
+robot.hear(/おみくじ/i, msg => {
+    const user_id = msg.message.user.id;
+    const lots = ['大吉', '吉', '中吉', '末吉', '凶'];
+    const lot = lots[Math.floor(Math.random() * lots.length)];
+    msg.send(`${lot}, <@${user_id}>`);
+  });
 };
+
